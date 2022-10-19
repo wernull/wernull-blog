ef0dc48e4bd327b307c09258872d9ed2811e5190
Kyle Werner <kyle@wernull.com>
2022-10-18 22:08:37 -0500

    clean up thumbnails and header, bash script for deploys

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,9 @@
-## Personal blog for Kyle Werner
-
-### Quick links
-
-`rake new_post['title']`
-
-### Dev server
-
-`bundle exec jekyll serve`
-
-## Deploy
-
-`bash deploy.sh`
-
-### Build
-
-`JEKYLL_ENV=production bundle exec jekyll build`
-
-### Sync to S3
+#!/bin/bash
+echo "Build project"
+JEKYLL_ENV=production bundle exec jekyll build
+echo "Sync to S3"
 aws s3 sync $HOME/documents/github/wernull-blog/public s3://wernull.com --profile wernull
-
-### Clear cloudfront cache
+echo "Clear cloudfront cache"
 aws cloudfront create-invalidation --distribution-id E166S0HG6YKT5Q --paths "/*" --profile wernull
 aws cloudfront create-invalidation --distribution-id E2NQH92HXEFZCN --paths "/*" --profile wernull
+echo "Deploy Complete"
